cfb027e0e0a6b4a568fb1ef8091bb897bcc95e17
puneethirvichetty <puneeth2011@gmail.com>
2016-10-12 15:57:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,3 +16,15 @@ button.onclick= function() {
     request.open('GET','http://puneethirvichetty.imad.hasura-app.io/',true);
     request.send(null);
 };
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit= document.getElementById('submit_btn');
+submit.onclick=function() {
+    var names=['name','name1','name2'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list='<li>'+names[i]+</li>;
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerhtml=list; 
+};
